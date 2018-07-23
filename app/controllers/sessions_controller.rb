124f872270e155b7b0149195feb2322f6f82f7b5
Petya Djanovska <petya.djanovska@gmail.com>
2018-07-23 23:14:53 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
+    binding.pry
     if !params[:name].blank?
       session[:name] = params[:name]
       redirect_to root_path
