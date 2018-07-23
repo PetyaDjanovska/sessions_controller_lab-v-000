94133098e8d78e648f738f60ab7be995656e3bd1
Petya Djanovska <petya.djanovska@gmail.com>
2018-07-23 23:19:42 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,6 @@ class SessionsController < ApplicationController
 
   def create
     if !params[:name].blank?
-          byebug
       session[:name] = params[:name]
       redirect_to root_path
     else
