6802680574b868978a319c5c8f57599d63be821c
Petya Djanovska <petya.djanovska@gmail.com>
2018-07-23 22:00:49 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if params[name].exists?
+    if params[:name].exists?
       session[:name] = params[:name]
       redirect_to root_path
     else
       redirect_to lodin_path
+    end
   end
 
   def destroy
