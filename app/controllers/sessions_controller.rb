4944e6c8e0098ed43bbff2a6a7a1870c1b29facf
Petya Djanovska <petya.djanovska@gmail.com>
2018-07-23 23:21:45 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
       session[:name] = params[:name]
       redirect_to root_path
     else
-      redirect_to login_path
+      redirect_to sessions_new_path
     end
   end
 
